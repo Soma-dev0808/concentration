9a90c3ca7d5825bc0385916fe9e29272010d1891
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-09-14 15:37:31 -0700

    Added vite in project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import Table from './components/Table';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='main'>
+        <Table />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import './Table.scss';
+
+// Table for concentration.
+const Table: FC = () => {
+    return (
+        <div>Table</div>
+    );
+};
+
+export default Table;
